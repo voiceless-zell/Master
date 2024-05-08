faa476676171e4cf665e0228911c89c73e9aa15d
voiceless-zell <peter.bouchard2893@proton.me>
2024-05-08 16:40:54 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Home Manager configuration of zell";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland = {
+        url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+  };
+
+  outputs = { nixpkgs, home-manager, self, ... }@inputs:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; config.allowunfree = true;};
+    in {
+      homeConfigurations."zell" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix 
+        inputs.hyprland.homeManagerModules.default];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
---
+++ packages/default.nix
@@ -0,0 +1,56 @@
+{ inputs
+, pkgs
+, config
+, lib
+, ...
+}: 
+  
+  with lib;
+    let
+      cfg = config.modules.packages;
+      star-citizen = pkgs.star-citizen.override (prev: {
+          wineDllOverrides = prev.wineDllOverrides ++ [ "dxgi=n" ];
+        });
+    in {
+      
+    options.modules.packages = { enable = mkEnableOption "packages"; };
+    config = mkIf cfg.enable {
+        home.packages = with pkgs; [
+      # others
+      bitwarden
+      p7zip
+      neofetch
+      openssl
+      libnotify
+      pamixer
+      xfce.thunar
+      pavucontrol
+      wget
+      unzip
+      gparted
+      mpv
+      playerctl
+      qalculate-gtk
+      htop
+      fzf
+      ffmpeg
+      discord
+      eza
+      obsidian
+      glib
+      #programming
+      nix-prefetch-github
+      #Tmux things stdin/write to file
+      moreutils
+      libcxxStdenv
+# security
+    x11_ssh_askpass
+#lazyvim
+      fd
+      gcc
+      tree-sitter
+      ruby
+      nodejs
+];
+    };
+}
